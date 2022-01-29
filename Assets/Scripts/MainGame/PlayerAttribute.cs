8b6e762feb16d49c15ae367d9ec23c57f067db63
JulienDelaunay <julien.delaunay034@gmail.com>
2022-01-29 17:12:57 +0100

    Add callback when switch color

--- Assets/Scripts/MainGame/PlayerAttribute.cs
+++ Assets/Scripts/MainGame/PlayerAttribute.cs
@@ -1,23 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public static class PlayerAttribute
 {
-    public static bool color = false;
-
-    [SerializeField]
+    #region Current State
     public static int life = 3;
-
-    [SerializeField]
     public static float speed = 1;
-
-    [SerializeField]
     public static float distance = 10;
+    private static bool m_Color = false;
+    #endregion
 
+
+    #region Properties
+    public static bool color {
+        get => m_Color;
+        set {
+            m_Color = value;
+            onColorSwitch?.Invoke();
+        }
+    }
+    
+    #endregion
+
+
+    #region Events
+    public static event Action onColorSwitch; 
+    #endregion
+    
+    
+    #region Behaviour
     public static void loseLife()
     {
         life--;
         Debug.Log("Hit taken");
     }
+    #endregion
 }
