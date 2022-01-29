82ac41665e191e84cdbdaa12d8963cbbc1c0c319
Fx73 <fx73000@yahoo.fr>
2022-01-29 18:58:19 +0100

    Synchro element

--- Assets/Scripts/MainGame/Obstacles/ObstacleAttribute.cs
+++ Assets/Scripts/MainGame/Obstacles/ObstacleAttribute.cs
@@ -11,6 +11,9 @@ public class ObstacleAttribute : MonoBehaviour
     void Start()
     {
         color = Random.value > 0.5f;
+        if (color)
+            gameObject.GetComponent<Renderer>().material.color = Color.cyan;
+
     }
 
     // Update is called once per frame
