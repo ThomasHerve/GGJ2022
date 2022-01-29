f6df895f6a24334a5d5c236e2d691d741273ce70
Lumosc <benoit.debief@free.fr>
2022-01-29 17:12:54 +0100

    Creation of High score panel

---
+++ Assets/UI/Scripts/HighScore.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HighScore : MonoBehaviour
+{
+
+    public TextMeshProUGUI ScorePositionPerso;
+    public TextMeshProUGUI ScoreValeurPerso;
+
+    public TextMeshProUGUI ScorePosition;
+    public TextMeshProUGUI ScoreValeur;
+
+
+
+}
