977e3a73c23bd1b32cf17cdda3258fe10f2fe9aa
JulienDelaunay <julien.delaunay034@gmail.com>
2022-01-30 17:28:41 +0100

    Volume management integration

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,37 +1,57 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour {
+	#region Settings
 	public AudioMixer globalMixer = null;
 	public float menuLowPassFilterAmount = 0.5f;
-	
+	#endregion
+
+
+	#region Currents
+	private float m_CurrentLowPassFilterRatio = 1f;  
+	#endregion
+
+	#region Callbacks
 	private void OnEnable() {
 		GameManager.onGameStart += OnGameStart;
 		GameManager.onGameEnd += OnGameEnd;
 	}
-
 	private void Start() {
 		SetLowPassFilterAmount(menuLowPassFilterAmount);
 	}
 
 	private void OnGameStart() {
-		SetLowPassFilterAmount(1f);
+		DoLowPassFilterFade(1f, 0.6f);
 	}
 
 	private void OnGameEnd() {
-		SetLowPassFilterAmount(menuLowPassFilterAmount);
+		DoLowPassFilterFade(menuLowPassFilterAmount, 0.3f);
 	}
 
-	private void SetLowPassFilterAmount(float ratio) {
-		globalMixer.SetFloat("MusicLowPass", Mathf.Lerp(10f, 22000f, ratio));
-	}
-	
-	public void SetVolume(float volume) {
-		
-	}
-	
 	private void OnDisable() {
 		GameManager.onGameStart += OnGameStart;
 		GameManager.onGameEnd += OnGameEnd;
 	}
+	#endregion
+
+	public void SetVolume(float volume) {
+		globalMixer.SetFloat("Volume", Mathf.Lerp(-80f, 0f, volume));
+	}
+
+	#region Music
+	private void DoLowPassFilterFade(float targetVal, float duration) {
+		DOTween.To(GetLowPassFilterAmount, SetLowPassFilterAmount, targetVal, duration);
+	}
+	
+	private void SetLowPassFilterAmount(float ratio) {
+		m_CurrentLowPassFilterRatio = ratio;
+		globalMixer.SetFloat("LowPass", Mathf.Lerp(10f, 22000f, ratio));
+	}
+
+	private float GetLowPassFilterAmount() {
+		return m_CurrentLowPassFilterRatio;
+	}
+	#endregion
 }
